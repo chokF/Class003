27828621c230456c133ea9b589c4d9e5d9dba708
gitignore <>
2019-04-22 11:51:37 +0800

    edit

--- Class003/ViewController.swift
+++ Class003/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbMessage: UILabel!
+    
+    @IBAction func btnPushClicked(_ sender: UIButton) {
+        lbMessage.text = "Hi, Xcode"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
